cb53106a1fba727ae8170d34b01254dddca29376
Boris Skert <boris.skert@gmail.com>
2022-06-25 14:04:23 +0200

    Solve 'Multiply' kata

---
+++ src/multiply/bug/fix.clj
@@ -0,0 +1,11 @@
+(ns multiply.bug.fix)
+
+;https://www.codewars.com/kata/50654ddff44f800200000004/train/clojure
+(defn multiply [a b]
+  (* a b)
+  )
+
+;also a solution:
+;(def multiply *)
+;or
+;(def multiply #(* % %2))

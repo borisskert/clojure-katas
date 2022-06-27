9a61144d43a975aa150a243982b80b33a5cc53d1
Boris Skert <boris.skert@gmail.com>
2022-06-27 20:20:11 +0200

    Solve 'Convert a Number to a String!' kata (#4)

---
+++ src/stringify.clj
@@ -0,0 +1,9 @@
+(ns stringify)
+
+;https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/clojure
+(defn number-to-string [num]
+  (str num)
+  )
+
+;Again what learned:
+;(def number-to-string str)
